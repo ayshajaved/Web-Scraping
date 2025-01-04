8e4338554edfdc97d087a2147f38a936347e9039
ayshajaved <ayshajavd@gmail.com>
2025-01-05 00:49:59 +0500

    Objects added

--- 2_request.py
+++ 2_request.py
@@ -8,14 +8,19 @@ response = requests.get(url)
 There are 4 objects of beautifulsoup
 -->tags tags of the html doc
 -->navigable strings content in the tags
--->beautiful soup
+-->beautiful soup 
 -->comments A special type of NavigableString that represents comments in the HTML or XML document.
 -->ResultSet object, which is essentially a list of all matching elements.
 '''
 if response.status_code == 200:
     print("Success")
     content = response.content
-    html = BeautifulSoup(content, 'html.parser')
+    html = BeautifulSoup(content, 'html.parser') #html is the beautiful soup object having following methods
+    #html.head                            
+    #html.body
+    #html.find()
+    #html.find_all()
+
     print(html)            #gives the incomprehensible html content
     print(html.prettify()) #gives the beautiful and clean html that is readable
     print(html.p)          #gives the first paragraph
