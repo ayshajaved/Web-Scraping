a2b1f87bef4f4a1b31d366b73e3c6824cb87342d
ayshajaved <ayshajavd@gmail.com>
2025-01-04 16:14:03 +0500

    Wikipedia Paragraph

--- main.py
+++ main.py
@@ -2,5 +2,13 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+url = "https://en.wikipedia.org/wiki/Web_scraping"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Extract the first paragraph of wikipedia
+first_paragraph = soup.find('p').text
+print(first_paragraph)
+
 
 
