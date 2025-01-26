63f75ddfef9296922f7e87e877c3aa8804dac73d
ayshajaved <ayshajavd@gmail.com>
2025-01-26 08:44:44 +0500

    Testing a simple add function by unittest

--- selenium/3_UnittestModule.py
+++ selenium/3_UnittestModule.py
@@ -5,3 +5,27 @@ and is free from errors. It is commonly used in both small-scale projects and la
 to improve code reliability.
 '''
 import unittest
+#testing a simple add formula
+def add(x, y):
+    return x + y
+
+class TestAdd(unittest.TestCase):   
+    #test case 1: test with positive numbers
+    def test_add_positive_numbers(self):
+        self.assertEqual(add(5, 7), 12)
+    
+    #test case 2: test with negative numbers
+    def test_add_negative_numbers(self):
+        self.assertEqual(add(-5, -7), -12)
+
+    #test case 3: test with mixed numbers
+    def test_add_mixed_numbers(self):
+        self.assertEqual(add(-5, 7), 2)
+
+    #test case 4: test with zero
+    def test_add_zero(self):
+        self.assertEqual(add(5, 0), 5)
+if __name__ == '__main__':
+    unittest.main()
+
+#printing ok if all test cases pass
